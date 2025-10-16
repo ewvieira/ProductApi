3a67e7705f3840d9109e5869628c110ae2bd9d4d
unknown <ewvieira@hotmail.com>
2025-10-16 09:43:45 -0300

    Limpeza de codigo desnecessario

--- ProductApi/Services/IProductService.cs
+++ ProductApi/Services/IProductService.cs
@@ -1,6 +1,4 @@
 using ProductApi.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ProductApi.Services
 {
--- ProductApi/Services/ProductService.cs
+++ ProductApi/Services/ProductService.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ProductApi.Data;
 using ProductApi.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ProductApi.Services
 {
